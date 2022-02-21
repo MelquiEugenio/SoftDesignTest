e583acc00316c9ec92a0517fee2db0e5540550a1
Melquisedeque Silva <melquisedeque.silva@wolk.com.br>
2022-02-21 00:41:14 -0300

    REFACTOR: Added validations.

--- app/src/main/java/br/com/wolk/softdesign_teste/view/MainActivity.kt
+++ app/src/main/java/br/com/wolk/softdesign_teste/view/MainActivity.kt
@@ -1,6 +1,10 @@
 package br.com.wolk.softdesign_teste.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -17,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel: EventsViewModel by viewModels()
 
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+        val errorImageView = findViewById<ImageView>(R.id.error_image_view)
         val recyclerview = findViewById<RecyclerView>(R.id.recycler_view)
         val adapter = EventsListAdapter(viewModel.events.value, viewModel, this)
 
@@ -24,7 +30,18 @@ class MainActivity : AppCompatActivity() {
         recyclerview.adapter = adapter
 
         viewModel.events.observe(this) {
-            adapter.updateData(it)
+            if (it != null) {
+                progressBar.visibility = View.GONE
+                adapter.updateData(it)
+            } else {
+                progressBar.visibility = View.GONE
+                errorImageView.visibility = View.VISIBLE
+                Toast.makeText(
+                    this,
+                    "Algo impediu a lista de carregar. Tente novamente.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
